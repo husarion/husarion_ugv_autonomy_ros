3747b7cb6e8376c3675a202af12e13f32c2469ec
kmakd <kmakd197@gmail.com>
2025-06-18 10:51:14 +0200

    fix docking

--- husarion_ugv_docking/include/husarion_ugv_docking/charging_dock.hpp
+++ husarion_ugv_docking/include/husarion_ugv_docking/charging_dock.hpp
@@ -95,9 +95,9 @@ public:
    * @brief Method to obtain the refined pose of the dock, usually based on
    * sensors
    * @param pose The initial estimate of the dock pose.
-   * @param id The frame of the initial estimate.
+   * @param frame The frame of the initial estimate.
    */
-  bool getRefinedPose(PoseStampedMsg &pose, std::string /* id */) override final;
+  bool getRefinedPose(PoseStampedMsg &pose, std::string frame) override final;
 
   /**
    * @brief Have we made contact with dock? This can be implemented in a variety
